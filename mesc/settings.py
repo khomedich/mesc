43c3023965890788698dc19be101314652a29a88
Oleksandr Khomenko <khomenko.ov-uds@ua.energy>
2023-01-15 23:05:23 +0200

    initial commit

--- mesc/settings.py
+++ mesc/settings.py
@@ -78,8 +78,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mesc',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'your_db_password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
